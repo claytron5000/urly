42c9e615c59301c4f5a042534d392747f2af6f97
Clayton Salem <claytonjsalem@gmail.com>
2019-08-25 15:29:30 -0400

    Move to api

---
+++ app/controllers/api/v1/urls_controller.rb
@@ -0,0 +1,67 @@
+class Api::V1::UrlsController < ApplicationController
+
+    @@alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.split('')
+    @@base = @@alphabet.length
+
+    def new
+        @url = Url.new
+    end
+
+    def show
+
+    end
+
+    def create
+        if (Url.where("target_url = ?", params[:url][:target_url]).exists?)
+            # render plain: params[:url]
+            @url = Url.where("target_url = ?", params[:url][:target_url]).first()
+            render json: @url
+            # render 'show'
+        else
+            # render plain: params[:url]
+            @url = Url.new(params.require(:url).permit(:target_url))
+            @url.save
+            @url.shortened_url = convertTo62(@url.id)
+            @url.save
+            render json: @url
+            # render 'show'
+        end
+    end
+
+    def goto
+        @shortened = params[:shortened_url]
+        @index = convertFrom62(@shortened)
+        @url = Url.find_by_id(@index)
+        if @url != nil
+            redirect_to @url.target_url
+        else
+            redirect_to request.base_url
+        end
+    end
+
+    private
+        def convertTo62(number)
+            
+            @remander = number % @@base
+            @result = @@alphabet[@remander]
+            @quotient = number/@@base
+            while (@quotient != 0)
+                @remander = @quotient % @@base
+                @quotient = @quotient/@@base
+                @result = @@alphabet[@remander].to_s + @result
+            end
+            return @result
+        end
+    
+        private 
+            def convertFrom62(number)
+                
+                @result = 0
+                number.split('').each do |num|
+                    @result = @@base * @result + @@alphabet.index(num)
+                end
+                
+                return @result;
+            end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'urls#new'
+  # root 'urls#new'
+  root to: 'home#index'
   get 't/:shortened_url', to: 'urls#goto'
-  resources :urls, only: [:create]
+  namespace :api do
+    namespace :v1 do
+      resources :urls, only: [:create]
+    end
+  end
   # @todo instead of redirecting gets, we can put the @url in a cookie, re-render the root and conditionally load the @url
-  get '/urls', to: redirect('/')
+  # get '/urls', to: redirect('/')
 
 end
